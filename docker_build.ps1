d88950cf737c5d1cf5e7339633abc595499d680f
Nick Hernandez <nickhernandez@djlactose.com>
2025-04-08 09:04:18 -0400

    Fixed a permissions issue where files couldn't be written out to the mount location.

---
+++ docker_build.ps1
@@ -0,0 +1,2 @@
+docker build -t djlactose/megawebdav ./
+docker push djlactose/megawebdav
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 /usr/bin/mega-cmd-server &
 /usr/bin/mega-login $username $password
 /usr/bin/mega-webdav --public /
+chmod 666 /mnt
 if [ $sync = true ] ; then
     uuid > /etc/machine-id
     /usr/bin/mega-sync /mnt /
